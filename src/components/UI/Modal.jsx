3012d77c8107e4d769727d9a0bb7918e98db54f6
Stavros <snikoletopoulos@hotmail.com>
2021-11-12 01:34:39 +0200

    Create Modal components

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import styles from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+const Backdrop = () => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = props => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = props => {
+  return (
+    <>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
