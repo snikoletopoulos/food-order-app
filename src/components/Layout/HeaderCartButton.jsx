6c662002c2d1242a3b74383f097a31c3e83bd6ea
Stavros <snikoletopoulos@hotmail.com>
2021-11-13 15:17:19 +0200

    Load dynamicly the numberOfCartItem

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,21 @@
 import styles from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = props => {
+  const cartContext = useContext(CartContext);
+  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
