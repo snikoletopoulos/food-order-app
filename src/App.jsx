a06e4c511e4a17fd2c45d63212760f1e246bd06f
Stavros <snikoletopoulos@hotmail.com>
2021-11-13 15:11:18 +0200

    Create CartContext for managing the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import CartProvider from './store/CartProvider'
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
