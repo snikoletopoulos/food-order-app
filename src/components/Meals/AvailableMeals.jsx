baca981dd2445bd61c338ad3849e87db3b363787
Stavros <snikoletopoulos@hotmail.com>
2021-11-20 20:58:25 +0200

    Add axios and fetch data from firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,41 @@
+import { useState, useEffect } from "react";
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import axios from "axios";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `${import.meta.env.VITE_FIREBASE_API_URL}meals.json`
+        );
+
+        const loadedMeals = [];
+
+        for (const key in response.data) {
+          loadedMeals.push({
+            ...response.data[key],
+            id: key,
+          });
+        }
+
+        setMeals(loadedMeals);
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoading(false);
+    })();
+  }, []);
+
+  }
+
+  const mealsList = meals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
