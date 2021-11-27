eb8e2bbe276008daef2337b042d2f417c1e37688
Stavros <snikoletopoulos@hotmail.com>
2021-11-27 15:11:18 +0200

    Add prop types

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,13 +1,21 @@
 import styles from "./Input.module.css";
 import { forwardRef } from "react";
 
-const Input = forwardRef((props, ref) => {
+const Input = forwardRef((props: Props, ref) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} ref={ref} />
+      <input {...props.input} id={props.input.id} type="text" ref={ref} />
     </div>
   );
 });
 
 export default Input;
+
+interface Props {
+  input: {
+    id: string;
+    name: string;
+  };
+  label: string;
+}
